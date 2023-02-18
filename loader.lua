5870a1d38b9353cae08289a7e81f44bcae0b9433
2aliveissocrazy <122767236+2aliveissocrazy@users.noreply.github.com>
2023-02-17 23:06:58 -0500

    Update and rename loader to loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 6516141723 then
+    local message = "join a game first :)"
+    local messageGui = game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "GlitchHubV3";
+    Text = message;
+    Duration = 5;
+})
+    end
+if game.PlaceId == 6839171747 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/2aliveissocrazy/glitchhubv3/main/doors/hotel.lua'))()
+end
